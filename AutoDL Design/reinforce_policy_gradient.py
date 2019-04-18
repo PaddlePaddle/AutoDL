f0e66c4869c66b99c425438ad5c06b296f52d73a
qinduohao <qinduohao@baidu.com>
2019-04-18 11:41:05 +0800

    fix encoding typo

--- AutoDL Design/reinforce_policy_gradient.py
+++ AutoDL Design/reinforce_policy_gradient.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- encoding:utf-8 -*-
+# -*- coding:utf-8 -*-
 # Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserve.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
